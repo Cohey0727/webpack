4eadf7d03d52460e8436b81f2bc90bfe63e5e4cf
Cohey0727 <ohayousagi.ac.kook0727@gmail.com>
2019-12-21 19:30:52 +0900

    feature/mini-css-extract-plugin add minify css

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './style-css.css';
+import './style-scss.scss';
 
 ReactDOM.render(
   <div>HelloReact</div>,
   document.getElementById('root')
-)
+);
